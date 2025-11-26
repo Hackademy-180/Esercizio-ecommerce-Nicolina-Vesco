0615b52a9922fb8454e06f943a1bd70139b4ec53
Nicolina <nicolina.vesco@gmail.com>
2025-11-26 17:29:30 +0100

    eccomerce

---
+++ main.js
@@ -0,0 +1,53 @@
+let nav = document.querySelector("#nav")
+let navlink = document.querySelectorAll(".nav-link")
+let imglogo = document.querySelector(".logo")
+let icon = document.querySelector(".icon-right")
+console.dir(imglogo);
+
+
+window.addEventListener("scroll", () => {
+
+    let scrolled = window.scrollY;
+    if (scrolled > 0) {
+        nav.classList.add("nav-custom2");
+        imglogo.src = "http://127.0.0.1:5500/media/logored.png";
+
+        navlink.forEach((link) => {
+            console.log(link);
+            
+            link.style.color ="rgb(196,0,0)";
+            link.addEventListener = ("mouseenter", () => {
+                link.style.color ="rgb(254,254,254)"
+            })
+            link.addEventListener("mouseleave", () => {
+                link.style.color ="rgb(10,10,10)";
+            })
+        })
+    } else {
+        nav.classList.remove("nav-custom2");
+        imglogo.src = "http://127.0.0.1:5500/media/logo.png";
+        navlink.forEach((link) => {
+            link.style.color = "rgb(10,10,10)";
+        })
+        link.addEventListener("mouseenter", () => { link.style.color = "rgb(254,254,254)" })
+        link.addEventListener("mouseleave", () => { link.style.color = "rgb(10,10,10)" })
+    }
+
+})
+
+
+// nav.classList.add("nav-custom2");
+// changeNavbar("nav-custom", "logo", "rgb(254,254,254)", "rgb(196,0,0)");
+// }else{
+// nav.classList.remove("nav-custom2");
+// changeNavbar( "logored", "rgb(133,133,133)", "rgb(196,0,0)");
+// }})
+
+// function changeNavbar(logo, color1, color2){
+// logo.src= `https://127.0.0.1:5500/index.html/media/${logo}.png`;
+// link.style.color=color1
+// link.addEventListener("mouseenter", ()=>{ link.style.color=color2 })
+// link.addEventListener("mouseleave", ()=>{ link.style.color= color1
+//  })
+
+// }
