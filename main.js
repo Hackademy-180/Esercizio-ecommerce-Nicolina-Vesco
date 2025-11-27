6bbcc94d0ab199842e16dfb39429a6d1c179f1c9
Nicolina <nicolina.vesco@gmail.com>
2025-11-27 09:13:56 +0100

    Piccole correzioni su javascript

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let nav = document.querySelector("#nav")
 let navlink = document.querySelectorAll(".nav-link")
 let imglogo = document.querySelector(".logo")
-let icon = document.querySelector(".icon-right")
+let icon = document.querySelectorAll(".icon-right")
 console.dir(imglogo);
 
 
@@ -13,14 +13,22 @@ window.addEventListener("scroll", () => {
         imglogo.src = "http://127.0.0.1:5500/media/logored.png";
 
         navlink.forEach((link) => {
-            console.log(link);
-            
-            link.style.color ="rgb(196,0,0)";
-            link.addEventListener = ("mouseenter", () => {
+            link.style.color ="rgb(254,254,254)";
+            link.addEventListener("mouseenter", () => {
                 link.style.color ="rgb(254,254,254)"
             })
             link.addEventListener("mouseleave", () => {
                 link.style.color ="rgb(10,10,10)";
+            })
+        })
+         icon.forEach((icona) => {
+            
+            icona.style.color ="rgb(254,254,254)";
+            icona.addEventListener("mouseenter", () => {
+                icona.style.color ="rgb(10,10,10)"
+            })
+            icona.addEventListener("mouseleave", () => {
+                icona.style.color ="rgb(254,254,254)";
             })
         })
     } else {
@@ -28,9 +36,14 @@ window.addEventListener("scroll", () => {
         imglogo.src = "http://127.0.0.1:5500/media/logo.png";
         navlink.forEach((link) => {
             link.style.color = "rgb(10,10,10)";
+            link.addEventListener("mouseenter", () => { link.style.color = "rgb(254,254,254)" })
+            link.addEventListener("mouseleave", () => { link.style.color = "rgb(10,10,10)" })
         })
-        link.addEventListener("mouseenter", () => { link.style.color = "rgb(254,254,254)" })
-        link.addEventListener("mouseleave", () => { link.style.color = "rgb(10,10,10)" })
+        icon.forEach((icona) => {
+            icona.style.color = "rgb(10,10,10)";
+            icona.addEventListener("mouseenter", () => { icon.style.color = "rgb(254,254,254)" })
+            icona.addEventListener("mouseleave", () => { icon.style.color = "rgb(10,10,10)" })
+        })
     }
 
 })
